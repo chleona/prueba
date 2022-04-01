411118f904c4fb6580d079537e10368c1e6574e2
hugol <hugol@LAPTOP-4K43RFRV>
2022-04-01 09:19:13 -0500

    Fuentes Iniciales

---
+++ src/main/java/com/example/prueba_tcs/PruebaTcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prueba_tcs/PruebaTcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
